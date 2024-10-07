501a46f31f8adc0d97e9a67850ccac811141b0e8
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-07 17:30:25 +0200

    docs: method in human code

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -6,10 +6,13 @@ const word = 'javascript';
 // .includes(a e i o u)
 
 // Dichiara la funzione qui.
-
+function vowelCount() {
+    // crea variabile che abbia le parti da cercare (vocali)
+    // cerca metodo per verificare che le parti da cercare corrispondano
+    // stampa il risultato
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+vowelCount(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
