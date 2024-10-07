0c71a7ee1ba67a01bc5a5f0285b6c77db1ba279f
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-07 17:50:21 +0200

    docs: continue includes method with loop for plus count

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,12 +4,23 @@ const word = 'javascript';
 
 // metodi
 // .includes(a e i o u)
+// conto dentro il conto
 
 // Dichiara la funzione qui.
-function vowelCount() {
+function vowelCount(word) {
     // crea variabile che abbia le parti da cercare (vocali)
+    let vowel = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
     // cerca metodo per verificare che le parti da cercare corrispondano
+    for (i = 0; i < word.length; i++) {
+        if (vowel.includes(word[i])){
+            count++;
+        }
+    }
+    // verifica se le vocali corrispondono all'array di vowel (con match?)
+
     // stampa il risultato
+    console.log(count);
 }
 
 // Invoca la funzione qui e stampa il risultato in console
